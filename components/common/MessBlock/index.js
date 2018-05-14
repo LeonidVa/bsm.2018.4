1665e3e7caad1e1bd2a57c2bf75cb746b6eda0a6
Igor Data <igor.s.data@gmail.com>
2018-05-15 02:04:34 +0300

    BeSmarter transition just for fun

--- components/common/MessBlock/index.js
+++ components/common/MessBlock/index.js
@@ -4,28 +4,30 @@ import Color from 'color';
 
 import facebookMessenger from 'img/messegers/facebook-messenger.svg'
 import telegram from 'img/messegers/telegram.svg'
-import viber from 'img/messegers/viber.svg'
-import wechat from 'img/messegers/wechat.svg'
 import whatsapp from 'img/messegers/whatsapp.svg'
 import vk from 'img/messegers/vk.svg'
 import instagram from 'img/messegers/instagram.svg'
 
-//vk https://vk.com/im?sel=-129623016
-const color = Color('#ff8a65');
-
+const colors = [
+    '#d84315',
+    '#e64a19',
+    '#f4511e',
+    '#ff7043',
+    '#ff8a65',
+];
 const messegers = (messegersList) => (
     <div className="block-mess__list">
         {messegersList.map((item, index) => {
                 return (
-                        <a href={item.url}
-                           target="_blank"
-                           key={index}
-                           className="block-mess__item"
-                           style={{background: color.darken(index / 10).string()}}
-                           title={item.title}
-                        >
-                            <img src={item.icon} alt={item.title}/>
-                        </a>
+                    <a href={item.url}
+                       target="_blank"
+                       key={index}
+                       className="block-mess__item"
+                       style={{background:colors[index]}}
+                       title={item.title}
+                    >
+                        <img src={item.icon} alt={item.title}/>
+                    </a>
                 )
             }
         )

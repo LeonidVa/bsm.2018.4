003c5fe91f2ecb366b302bea853bd7e4e3991dc2
Igor Data <igor.s.data@gmail.com>
2018-05-16 02:23:29 +0300

    CI test

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pm2 stop besmarter
-pm2 start yarn --name "besmarter" -- run start
+pm2 start yarn --name "besmarter" --kill-timeout 30000 -- run start
--- update.sh
+++ update.sh
@@ -2,8 +2,10 @@
 rm -f /var/www/updates/besmarter
 
 path=/var/www/besmarter
-cd ${path}
 
+
+cd ${path}
+sh ./stop.sh
 rm -rf ${path}/.next/
 git reset --hard HEAD
 git pull origin master
@@ -11,3 +13,5 @@ chown -R www-data:www-data ${path}
 chmod -R 644 ${path}
 chmod -R  +X ${path}
 chmod 775 ${path}/uploads
+sh ./start.sh
+

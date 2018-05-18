85dd06098a0296ec863bdc8b0ab28b4264d35b86
slexx1234 <slexx1234@gmail.com>
2018-05-18 17:58:07 +0300

    Добавл условие

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-pm2 stop besmarter
-set -e
-pm2 start npm --name "besmarter" --kill-timeout 30000 -- run start
+
+if ! /c/Users/a.schepkin.OFFICE/AppData/Roaming/npm/node_modules/pm2/bin/pm2 restart besmarter
+then
+    set -e
+    /c/Users/a.schepkin.OFFICE/AppData/Roaming/npm/node_modules/pm2/bin/pm2 start npm --name "besmarter" --kill-timeout 30000 -- run start
+fi

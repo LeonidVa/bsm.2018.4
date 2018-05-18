67bef70f7174d1bb8d646f6445c09586fb269ac1
slexx1234 <slexx1234@gmail.com>
2018-05-18 17:58:28 +0300

    Забыл убрать

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if ! /c/Users/a.schepkin.OFFICE/AppData/Roaming/npm/node_modules/pm2/bin/pm2 restart besmarter
+if ! pm2 restart besmarter
 then
     set -e
-    /c/Users/a.schepkin.OFFICE/AppData/Roaming/npm/node_modules/pm2/bin/pm2 start npm --name "besmarter" --kill-timeout 30000 -- run start
+    pm2 start npm --name "besmarter" --kill-timeout 30000 -- run start
 fi

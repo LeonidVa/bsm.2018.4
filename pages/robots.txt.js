a277713f461a82e8da3718a421cea5fb2e53b150
slexx1234 <slexx1234@gmail.com>
2018-06-20 13:43:02 +0300

    Поправил robots.txt

---
+++ pages/robots.txt.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class extends React.Component {
+    static async getInitialProps ({ res }) {
+        if (!res) return {};
+        res.setHeader('Content-Type', 'application/javascript');
+        res.end(`User-agent: *
+Disallow: /`, 'utf8');
+        return {}
+    }
+
+    render () {
+        return null
+    }
+}

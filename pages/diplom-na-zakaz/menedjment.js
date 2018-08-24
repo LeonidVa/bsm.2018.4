484f565480a7bb05d97d76c772a16c201aebe80d
Shalser <shalser82@yandex.ru>
2018-08-24 15:35:26 +0300

    поменял оценить на заказать

--- pages/diplom-na-zakaz/menedjment.js
+++ pages/diplom-na-zakaz/menedjment.js
@@ -263,7 +263,7 @@ const page = () => (
             </Video>
 
             <a name="form" id="form"/>
-            <OrderForm title="Заказать работу" redForm={true} buttonLabel="Оценить" fields={fields}/>
+            <OrderForm title="Заказать работу" redForm={true} buttonLabel="Заказать" fields={fields}/>
 
             <LinksBlock links={links}/>
         </div>

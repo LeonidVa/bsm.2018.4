74b926c26e6c4fc8604c9e8bc9a75c21cb94c3b2
Igor Data <igor.s.data@gmail.com>
2018-10-06 15:31:40 +0300

    "игорь ковыряет кнопочку"

--- components/common/CardPopInfo/index.jsx
+++ components/common/CardPopInfo/index.jsx
@@ -1,31 +1,53 @@
 import React from "react";
 import Link from "next/link";
-
+import {isMobile, isTablet} from 'react-device-detect';
 
 const CardPopInfo = ({title = "", text = "", url = "", description = []}) => {
-    let el = (
-        <div className="b-item">
-            <div className="b-item__content">
-                <div className="block-service__list-title">{title}</div>
-                {text}
+    const drawDesktop = () => {
+        const el = (
+            <div className="b-item">
+                <div className="b-item__content">
+                    <div className="block-service__list-title">{title}</div>
+                    {text}
+                </div>
+                <div className="b-item__hovered">
+                    <div className="b-item__hovered-text">{description}</div>
+                </div>
             </div>
-            <div className="b-item__hovered">
-                {description.map(item => (
-                    <div className="b-item__hovered-text">{item}</div>
-                ))}
-            </div>
-        </div>);
-    if (url !== "") {
-        el = (
-            <Link href="">
+        );
+        if (url === "") {
+            return <div className="block-service__list-item b-list-item">{el}</div>;
+        }
+        return (
+            <Link href={url}>
                 <a className="block-service__list-item b-list-item">
                     {el}
                 </a>
-            </Link>);
-    } else {
-        el = <div className="block-service__list-item b-list-item">{el}</div>
+            </Link>
+        );
+    };
+    const drawMobile = () => {
+        return (
+            <div className="block-service__list-item mobile">
+                <div className="block-service__list-title">{title}</div>
+                {text}
+                <div className="block-service__m-description">
+                    {description}
+                    <Link href={url}>
+                        <a className="block-service__m-description-button">
+                            Press me!
+                        </a>
+                    </Link>
+                </div>
+            </div>)
+    };
+
+    if (isMobile) {
+        return drawMobile();
     }
-    return el;
+    return drawDesktop();
+
+
 };
 
 export default CardPopInfo;

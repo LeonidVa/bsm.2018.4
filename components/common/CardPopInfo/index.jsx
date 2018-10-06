58c48a5f66d077647e2d000c264d876c319a54cc
Igor Data <igor.s.data@gmail.com>
2018-10-06 12:22:16 +0300

    "вариант игоря, рабочий пример"

--- components/common/CardPopInfo/index.jsx
+++ components/common/CardPopInfo/index.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import Link from "next/link";
 
-const CardPopInfo = ({ color, title, cost, days, description = [] }) => {
-  return (
-    <div className="b-item">
-      <div className="b-item__content">
-        <div className="block-service__list-title">{title}</div>
-        <p>{cost}</p>
-        <p>{days}</p>
-      </div>
-      <div className="b-item__hovered" style={{backgroundColor: color}}>
-        {description.map(item => (
-          <div className="b-item__hovered-text">{item}</div>
-        ))}
-      </div>
-    </div>
-  );
+
+const CardPopInfo = ({title = "", text = "", url = "", description = []}) => {
+    let el = (
+        <div className="b-item">
+            <div className="b-item__content">
+                <div className="block-service__list-title">{title}</div>
+                {text}
+            </div>
+            <div className="b-item__hovered">
+                {description.map(item => (
+                    <div className="b-item__hovered-text">{item}</div>
+                ))}
+            </div>
+        </div>);
+    if (url !== "") {
+        el = (
+            <Link href="">
+                <a className="block-service__list-item b-list-item">
+                    {el}
+                </a>
+            </Link>);
+    } else {
+        el = <div className="block-service__list-item b-list-item">{el}</div>
+    }
+    return el;
 };
 
 export default CardPopInfo;

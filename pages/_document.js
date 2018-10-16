070c37e8a37046de6793f39a0a7a376d7f36d3a5
Igor Data <igor.s.data@gmail.com>
2018-10-16 16:19:32 +0300

    statistics fixes

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
             <html>
             <Head>
                 <meta name="yandex-verification" content="49e34bd8a11a83fb"/>
+                <YMInitializer accounts={[yaID]} options={{defer: true}} version="2"/>
             </Head>
             <body>
                 <Main/>
@@ -23,7 +24,6 @@ export default class MyDocument extends Document {
                 <link href="https://fonts.googleapis.com/css?family=Ubuntu:300i,400,500,500i,700" rel="stylesheet"/>
                 <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
                 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
-                <YMInitializer accounts={[yaID]} options={{defer: true}} version="2"/>
             </body>
             </html>
         )

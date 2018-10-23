46ae698e780ab2291be70ea828b154f53e765df7
Shalser <shalser82@yandex.ru>
2018-10-23 17:37:53 +0300

    правка

--- components/common/CallAsk/index.js
+++ components/common/CallAsk/index.js
@@ -10,7 +10,8 @@ class BottomHelper extends Component {
             <div>
                 <section className="block-callask">
                     <div className="inner">
-                        <h2 className="block-callask__title">Остались вопросы?<br/>Мы ответим!</h2>
+                        <h2 className="block-callask__title">Остались вопросы?</h2>
+                        <h2 className="block-callask__title">Мы ответим!</h2>
                         <div className="block-callask__buttons">
                             <ToggleQuestionPopup targetID="question"><a className="block-callask__button">Задать вопрос</a></ToggleQuestionPopup>
                             <ToggleCallPopup targetID="call_me_bottom"><a className="block-callask__button">Заказать звонок</a></ToggleCallPopup>

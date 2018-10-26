ddd245b42f44ddf4c6a862a3df2739cc1d5b520d
Shalser <shalser82@yandex.ru>
2018-10-26 18:45:16 +0300

    rewrite robots.txt

--- pages/robots.txt.js
+++ pages/robots.txt.js
@@ -5,7 +5,6 @@ export default class extends React.Component {
     if (!res) return {};
     res.setHeader('Content-Type', 'application/javascript');
     res.end(`User-agent: *
-Disallow: https://2018.besmarter.ru/cdn-cgi/l/email-protection
 Disallow: https://www.cloudflare.com/sign-up?utm_source=email_protection
 Disallow: https://support.cloudflare.com/hc/en-us/articles/200170016-What-is-Email-Address-Obfuscation-
 Disallow: https://support.cloudflare.com/hc/en-us/categories/200275218-Getting-Started

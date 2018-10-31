d7a5984cb55dfcf171051a46be46131e84c654a1
Osipov Igor <osipov.kaluga@gmail.com>
2018-10-31 21:38:03 +0300

    try fix yandex metrica 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import React from "react";
 import App, { Container } from "next/app";
-import Router from "next/router";
-
-// import ym, { YMInitializer } from "react-yandex-metrika";
-//
-// const handleRouteChange = url => {
-//   ym("hit", url);
-// };
-
-Router.events.on("routeChangeComplete", handleRouteChange);
 
 export default class MyApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
@@ -26,11 +17,6 @@ export default class MyApp extends App {
 
     return (
       <Container>
-        {/*<YMInitializer*/}
-          {/*accounts={[132186]}*/}
-          {/*options={{ defer: true }}*/}
-          {/*version="2"*/}
-        {/*/>*/}
         <Component {...pageProps} />
       </Container>
     );

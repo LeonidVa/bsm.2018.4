58a2fef2f13c1f8fbab34b637196deac6581fa96
Igor Data <igor.s.data@gmail.com>
2018-11-20 22:02:35 +0300

    robots.txt for landing

--- pages/landings/diplom/robots.txt.js
+++ pages/landings/diplom/robots.txt.js
@@ -1,5 +1,16 @@
-const page = () => (`
+import React from 'react';
+
+export default class extends React.Component {
+  static async getInitialProps({res}) {
+    if (!res || res.end === undefined) return {};
+    res.end(`
 User-agent: *
 Disallow: /
-`);
-export default page;
+`, 'utf8');
+    return {}
+  }
+
+  render() {
+    return null
+  }
+}

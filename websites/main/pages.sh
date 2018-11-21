9636f9a325216afee7b6a58e4b87a8bc1124482a
Igor Data <igor.s.data@gmail.com>
2018-11-22 00:59:09 +0300

    two towers

--- websites/main/pages.sh
+++ websites/main/pages.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 BASENAME=`basename "${DIR}"`
-TARGET="${DIR}/pages"
-LINK=`realpath "${DIR}/../../pages"`
+#TARGET="${DIR}/pages"
+#LINK=`realpath "${DIR}/../../pages"`
 DOTNEXT=`realpath "${DIR}/../../.next"`
+
+set -x
+cd ../../
+TARGET="./websites/${BASENAME}/pages"
+LINK="./pages"
 echo removing "${LINK}"
 rm -rf "${LINK}"
 echo removing "${DOTNEXT}"
 rm -rf "${DOTNEXT}"
 echo creating a link to "${TARGET}" at "${LINK}"
 ln -s "${TARGET}" "${LINK}"
+ls -al
+cd ./pages
+ls -al
 echo Switched to ${BASENAME}

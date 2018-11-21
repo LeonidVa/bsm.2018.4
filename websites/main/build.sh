48784a634ebc7b931e7b23b97c4147221e2fb42e
Igor Data <igor.s.data@gmail.com>
2018-11-22 01:34:15 +0300

    two towers

--- websites/main/build.sh
+++ websites/main/build.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 echo i think im in ${DIR}
-sh "${DIR}/pages.sh"
-cd "${DIR}/../../"
+sh ./pages.sh
+cd ../..
 yarn run build
--- websites/main/pages.sh
+++ websites/main/pages.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 BASENAME=`basename "${DIR}"`
-#TARGET="${DIR}/pages"
-#LINK=`realpath "${DIR}/../../pages"`
+TARGET="${DIR}/pages"
+LINK=`realpath "${DIR}/../../pages"`
 DOTNEXT=`realpath "${DIR}/../../.next"`
-
-set -x
-cd ../../
-TARGET="./websites/${BASENAME}/pages"
-LINK="./pages"
 echo removing "${LINK}"
 rm -rf "${LINK}"
 echo removing "${DOTNEXT}"

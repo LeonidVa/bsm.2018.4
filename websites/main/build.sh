20e6839b7d9d1b0038075dbe3d0c0cc88a3967d2
Igor Data <igor.s.data@gmail.com>
2018-11-22 00:19:07 +0300

    two towers

--- websites/main/build.sh
+++ websites/main/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export NODE_ENV="production"
 set -e
+echo adin
+echo $1
+echo "${1}"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-sh ${DIR}/pages.sh
+sh "${DIR}/pages.sh"
 yarn run build

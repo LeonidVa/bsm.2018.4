f5e0f9af7f81881c4bed00a55e61a774622ccc16
Igor Data <igor.s.data@gmail.com>
2018-11-22 02:20:59 +0300

    two towers

--- websites/landing/build.sh
+++ websites/landing/build.sh
@@ -2,5 +2,6 @@
 export NODE_ENV="production"
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-sh ${DIR}/pages.sh
+chmod +x "${DIR}/*.sh"
+"${DIR}/pages.sh"
 yarn run build
--- websites/main/pages.sh
+++ websites/main/pages.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "in pages now"
-echo "bs0 is ${BASH_SOURCE[0]}"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 BASENAME=`basename "${DIR}"`
 TARGET="${DIR}/pages"

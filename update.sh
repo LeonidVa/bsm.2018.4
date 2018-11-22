3cc2eb4ed5421ded453e1bc99357dd9c2c56fa39
Igor Data <igor.s.data@gmail.com>
2018-11-22 16:37:00 +0300

    two towers

--- update.sh
+++ update.sh
@@ -168,9 +168,9 @@ restart_server () {
         chmod +x "${D_BUILD}/websites/${WEBSITE}/stop.sh"
         "${D_BUILD}/websites/${WEBSITE}/stop.sh"
       fi
-      rm -rf "${D_BUILD}.old.3"
-      mv "${D_BUILD}.old.2" "${D_BUILD}.old.3"
-      mv "${D_BUILD}.old.1" "${D_BUILD}.old.2"
+      rm -rf "${D_BUILD}.old.3" 2>/dev/null
+      mv "${D_BUILD}.old.2" "${D_BUILD}.old.3" 2>/dev/null
+      mv "${D_BUILD}.old.1" "${D_BUILD}.old.2" 2>/dev/null
       if mv "${D_BUILD}" "${D_BUILD}.before.1"
       then
           success "Old build moved to ${D_BUILD}.before.1"

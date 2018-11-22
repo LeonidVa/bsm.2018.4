b8bde6c6fb509ea38f74f8e0de7b75497794135f
Igor Data <igor.s.data@gmail.com>
2018-11-22 15:43:24 +0300

    two towers

--- websites/main/pages.sh
+++ websites/main/pages.sh
@@ -3,19 +3,11 @@ set -x
 CURRENTDIR="$( pwd )"
 DIR="$( dirname $( realpath "${0}" ) )"
 BASENAME=`basename "${DIR}"`
-TARGET="${DIR}/pages"
-LINK=`realpath "${DIR}/../../pages"`
-DOTNEXT=`realpath "${DIR}/../../.next"`
-echo removing "${LINK}"
-rm -rf "${LINK}"
-echo removing "${DOTNEXT}"
-rm -rf "${DOTNEXT}"
-echo current dir
-pwd
 cd "${DIR}"
-pwd
 cd ../..
 pwd
+rm -rf ./pages
+rm -rf ./.next
 ln -s "./websites/${BASENAME}/pages" "./pages"
 cd "${CURRENTDIR}"
 pwd

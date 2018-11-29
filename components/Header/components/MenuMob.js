a70e62110bdcc6c02891a3f1863cf487b17c2b1f
Sergey Andreev <aandreev.sm@gmail.com>
2018-11-29 19:09:43 +0300

    Исправил верстку меню

--- components/Header/components/MenuMob.js
+++ components/Header/components/MenuMob.js
@@ -6,7 +6,16 @@ import './MenuMob.scss';
 class MenuMob extends Component{
 
     state = {
-        showMenu: false
+        showMenu: false,
+        isShownDropDownMenu: false,
+    };
+
+    onShowDropDownMenu = () => {
+        this.setState((state, props) => {
+            return {
+                isShownDropDownMenu: !state.isShownDropDownMenu,
+            }
+        })
     }
 
     componentWillReceiveProps = (nextProps, nextState)=>{
@@ -17,11 +26,28 @@ class MenuMob extends Component{
     render(){
         return(
             <div className="menu-mob" style={{top: this.state.showMenu ? '0' : '-170%'}}>
-                <Link href="/price"><a className="menu-mob__title">Услуги и цены</a></Link>
                 <Link href="/about"><a className="menu-mob__title">О нас</a></Link>
                 <Link href="/garantii"><a className="menu-mob__title">Гарантии</a></Link>
                 <Link href="/contacts"><a className="menu-mob__title">Контакты</a></Link>
                 <Link href="/avtoram"><a className="menu-mob__title">Авторам</a></Link>
+                <Link href="/price"><a className="menu-mob__title">Цены</a></Link>
+                <a className="menu-mob__title" onClick={this.onShowDropDownMenu}>Услуги</a>
+                {this.state.isShownDropDownMenu &&
+                  <div className="menu-mob__drop-down">
+                      <Link href="/diplom-mba-na-zakaz"><a className="menu-mob__drop-down-el">Диплом МБА</a></Link>
+                      <Link href="/dissertaciya-na-zakaz"><a className="menu-mob__drop-down-el">Магистерская</a></Link>
+                      <Link href="/diplom-na-zakaz"><a className="menu-mob__drop-down-el">Дипломная работа</a></Link>
+                      <Link href="/razrabotka-diplomnogo-proekta"><a className="menu-mob__drop-down-el">Дипломный проект</a></Link>
+                      <Link href="/kursovaya-rabota-na-zakaz"><a className="menu-mob__drop-down-el">Курсовая работа</a></Link>
+                      <Link href="/zakazat-otchet-po-praktike"><a className="menu-mob__drop-down-el">Отчет по практике</a></Link>
+                      <Link href="/kontrolnaya-rabota-na-zakaz"><a className="menu-mob__drop-down-el">Контрольная</a></Link>
+                      <Link href="/referat-na-zakaz"><a className="menu-mob__drop-down-el">Реферат</a></Link>
+                      <Link href="/zakazat-esse"><a className="menu-mob__drop-down-el">Эссе</a></Link>
+                  </div>
+                }
+               
+              
+                
             </div>
         )
     }
